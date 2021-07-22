7e351690d53493a4f542900bec8100812bd6697e
Ivan-Danilov996 <ivandanilov996@yandex.ru>
2021-07-22 10:37:18 +0300

    create upload files

--- main.py
+++ main.py
@@ -3,13 +3,12 @@ import os
 
 url = 'https://api.vk.com/method/'
 TOKEN = '958eb5d439726565e9333aa30e50e0f937ee432e927f0dbd541c541887d919a7c56f95c04217915c32008'
-YATOKEN = 'AQAAAAArlzIkAADLW5pM2qBTp002qhWAmg8rGaU'
 
 
 class VK:
-    def __init__(self, token):
+    def __init__(self, token, id):
         self.params = {
-            'owner_id': '552934290',
+            'owner_id': id,
             'access_token': token,
             'v': '5.77',
             'album_id': 'profile',
@@ -19,6 +18,7 @@ class VK:
         self.photos = []
 
     def create_request(self, METHOD):
+        print('Получаем файлы из VK...')
         result = requests.get(url + METHOD, params=self.params)
         return result.json()
 
@@ -32,22 +32,24 @@ class VK:
         return self.photos
 
 
-vk = VK(TOKEN)
+input_id = input('Введите id пользователя: ')
+input_yatoken = input('Введите яндекс токен: ')
+vk = VK(TOKEN, input_id)
 data = vk.create_request('photos.get')
 photos = vk.create_photo_info(data)
-os.mkdir('photos')
-
 
 photo_names = []
 
 for photo in photos:
+    print(f'Создаем файл... {str(photo["name"]) + ".jpg"}')
     data = requests.get(photo['url'])
-    if photo['name'] in photo_names:
-        name = str(photo['date'])
+    if str(photo['name']) + '.jpg' in photo_names:
+        name = str(photo['date']) + '.jpg'
+        photo_names.append(str(photo['date']) + '.jpg')
     else:
-        name = str(photo['name'])
-        photo_names.append(photo['name'])
-    with open('photos/' + name + '.jpg', 'wb') as file:
+        name = str(photo['name']) + '.jpg'
+        photo_names.append(str(photo['name']) + '.jpg')
+    with open(name, 'wb') as file:
         for chunk in data.iter_content():
             file.write(chunk)
 
@@ -56,25 +58,29 @@ class YaUploader:
     def __init__(self, token):
         self.token = token
         self.headers = {'Accept': 'application/json', "Authorization": self.token}
+        self.folder = ''
 
-    def upload(self, file_path):
-        params = {'path': file_path}
-        response = requests.get('https://cloud-api.yandex.net/v1/disk/resources/upload', params=params,
-                                headers=self.headers)
-        put_url = response.json().get('href')
-        files = {'file': open(file_path, 'rb')}
-        response = requests.put(put_url, files=files, headers=headers)
-        print(response)
+    def upload(self, names):
+        for name in names:
+            print(f'Загрузка {name} на сервер')
+            params = {'path': 'photos/' + name}
+            response = requests.get('https://cloud-api.yandex.net/v1/disk/resources/upload', params=params,
+                                    headers=self.headers)
+            put_url = response.json().get('href')
+            files = {'file': open(name, 'rb')}
+            response = requests.put(put_url, files=files, headers=self.headers)
+            if response.status_code == 201:
+                print('Загрузка файла завершена')
+        print('Готово')
 
     def create_ya_folder(self):
+        print('Создаем папку на Яндекс диске')
         params = {'path': 'photos'}
-        response = requests.get('https://cloud-api.yandex.net/v1/disk/resources', params=params, headers=self.headers)
-        print(response.json())
+        response = requests.put('https://cloud-api.yandex.net/v1/disk/resources', params=params, headers=self.headers)
+        self.folder = response.json().get('href')
 
 
 if __name__ == '__main__':
-    pass
-    # uploader = YaUploader(YATOKEN)
-    # file_path = 'Ivan.txt'
-    # uploader.upload(file_path)
-    # uploader.create_ya_folder()
+    uploader = YaUploader(input_yatoken)
+    uploader.create_ya_folder()
+    uploader.upload(photo_names)
